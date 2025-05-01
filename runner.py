2fb6225a1b2ddd0b50d14a30539026e599cb4282
sukeshe17 <sukethshetty733@gmail.com>
2025-05-01 18:04:56 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ sys.dont_write_bytecode = True
 def main():
     # Customize the arguments for pytest run
     pytest_args = [
-        "tests/table3",                      # Folder containing your test cases
+        "tests/table7",                      # Folder containing your test cases
         "-v",                          # Verbose output
         "--tb=short",                  # Short traceback format
         "--capture=tee-sys",          # Capture and also print stdout/stderr
